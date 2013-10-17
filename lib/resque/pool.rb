54e43c7941ecda12155490a8aa54335478ebed2e
Ben Povlich <bpovlich@backupify.com>
2013-10-17 13:17:35 -0400

    Added logging to maintain_worker_count

--- lib/resque/pool.rb
+++ lib/resque/pool.rb
@@ -270,6 +270,7 @@ module Resque
     def maintain_worker_count
       all_known_queues.each do |queues|
         delta = worker_delta_for(queues)
+        log "#{Time.now.utc} - maintain_worker_count: delta: #{delta}, orphanted_worker_count: #{orphaned_worker_count}, all_known_queues_size: #{all_known_queues.size}" if delta != 0
         spawn_missing_workers_for(queues, delta) if delta > 0
         quit_excess_workers_for(queues, delta.abs)   if delta < 0
       end
@@ -284,10 +285,12 @@ module Resque
     # perhaps this means a class is waiting to be extracted
 
     def spawn_missing_workers_for(queues, delta)
+      log "#{Time.now.utc} - spawning #{delta} workers"
       delta.times { spawn_worker!(queues) }
     end
 
     def quit_excess_workers_for(queues, delta)
+      log "#{Time.now.utc} - quitting #{delta} workers"
       pids_for(queues)[0...delta].each do |pid|
         Process.kill("QUIT", pid)
       end
